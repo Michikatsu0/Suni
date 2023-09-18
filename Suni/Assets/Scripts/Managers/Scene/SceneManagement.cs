360d4a5f287acce7a5760541d910ec8a79f58df7
cheiraml <44727218+cheiraml@users.noreply.github.com>
2023-09-18 09:44:13 -0500

    Meditación y próximamente

--- Suni/Assets/Scripts/Managers/Scene/SceneManagement.cs
+++ Suni/Assets/Scripts/Managers/Scene/SceneManagement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum AppScene { LOGIN, REGISTER, HOME, MEDITATION, ANIM }
+public enum AppScene { LOGIN, REGISTER, HOME, MEDITATION, PUZZLES, GARDEN, CLOSET,  ANIM }
 
 public class SceneManagement : MonoBehaviour
 {
