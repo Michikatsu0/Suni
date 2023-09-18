65df917a52e27032d3568eed67dd3518ce0be85a
Sego <sebastian.segovia@upb.edu.co>
2023-09-18 12:05:47 -0500

    Fix Issue

---
+++ Suni/Assets/Scripts/Managers/Scene/Register/RegisterManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] uiElemetsList;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        uiElemetsList[0].SetActive(false);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
